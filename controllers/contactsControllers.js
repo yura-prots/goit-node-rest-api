679d7df954f7079fbe37d5565b4ba95f8d5a89c8
yura-prots <yuraprots@gmail.com>
2024-03-29 10:30:07 +0200

    create first controller

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,9 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
 
 export const getOneContact = (req, res) => {};
 
