02740ee4b8f91379a4862ee01b3620703889ec95
yura-prots <yuraprots@gmail.com>
2024-04-02 01:00:12 +0300

    database connection successful

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+const DB = process.env.DB_HOST;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
