5f1183b49f8b5d63643c96cb75422edb75c44b54
yura-prots <yuraprots@gmail.com>
2024-04-07 20:21:45 +0300

    add bcryptjs

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,16 +1,19 @@
+import bcrypt from "bcryptjs";
+
 import User from "../models/User.js";
 import HttpError from "../helpers/HttpError.js";
 
 export const register = async (req, res, next) => {
   try {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (user) {
       throw HttpError(409, "Email in use");
     }
 
-    const newUser = await User.create(req.body);
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ ...req.body, password: hashPassword });
 
     res.status(201).json({
       email: newUser.email,
