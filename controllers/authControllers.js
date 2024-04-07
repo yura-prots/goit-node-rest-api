6d160fd9c7c7e400ed921712ea65e4dab4302913
yura-prots <yuraprots@gmail.com>
2024-04-07 19:36:41 +0300

    add try-catch

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,11 +1,22 @@
 import User from "../models/User.js";
 import HttpError from "../helpers/HttpError.js";
 
-export const register = async (req, res) => {
-  const newUser = await User.create(req.body);
+export const register = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
 
-  res.status(201).json({
-    email: newUser.email,
-    subscription: newUser.subscription,
-  });
+    if (user) {
+      throw HttpError(409, "Email in use");
+    }
+
+    const newUser = await User.create(req.body);
+
+    res.status(201).json({
+      email: newUser.email,
+      subscription: newUser.subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
