3ebf5639669f5fc94c5b217d984c1b940f3cd335
yura-prots <yuraprots@gmail.com>
2024-04-07 16:10:08 +0300

    created authRouter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import authRouter from "./routes/authRouter.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use("api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
