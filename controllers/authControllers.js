01679f1782a72ea959ccbee4fd755c1ff62d438c
yura-prots <yuraprots@gmail.com>
2024-04-07 20:43:34 +0300

    create login controller

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -23,3 +23,30 @@ export const register = async (req, res, next) => {
     next(error);
   }
 };
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw HttpError(401, "Email is invalid");
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+
+    if (!passwordCompare) {
+      throw HttpError(401, "Not authorized");
+    }
+
+    const token = "exampletoken";
+
+    res.json({
+      token,
+      email: user.email,
+      subscription: user.subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { register } from "../controllers/authControllers.js";
+import { register, login } from "../controllers/authControllers.js";
 import { userRegisterSchema, userLoginSchema } from "../models/User.js";
 import validateBody from "../helpers/validateBody.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(userRegisterSchema), register);
+
+authRouter.post("/login", validateBody(userLoginSchema), login);
 
 export default authRouter;
