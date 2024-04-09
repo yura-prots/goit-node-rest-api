bb123ae246c59659d292bfcd724f16c80c8cd551
yura-prots <yuraprots@gmail.com>
2024-04-09 21:33:27 +0300

    create authenticate middleware

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -1,3 +1,28 @@
-const authenticate = async (req, res, next) => {};
+import jwt from "jsonwebtoken";
+
+import User from "../models/User.js";
+import { HttpError } from "../helpers/index.js";
+
+const SECRET = process.env.JWT_SECRET;
+
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    throw HttpError(401);
+  }
+
+  try {
+    const { id } = jwt.verify(token, SECRET);
+    const user = await User.findById(id);
+    if (!user) {
+      throw HttpError(401, "Not authorized");
+    }
+
+    next();
+  } catch (error) {
+    throw HttpError(401, error.message);
+  }
+};
 
 export default authenticate;
