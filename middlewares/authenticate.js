ec6195d727f351448d5fa6b239e68c42177bf543
yura-prots <yuraprots@gmail.com>
2024-04-16 11:58:27 +0300

    changed "throw" to "next"

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,21 +9,46 @@ const authenticate = async (req, _, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer" && token === null) {
-    next(HttpError(401, "Not authorized"));
+    throw HttpError(401, "Not authorized");
   }
 
   try {
     const { id } = jwt.verify(token, SECRET);
     const user = await User.findById(id);
     if (!user || !user.token || user.token !== token) {
-      next(HttpError(401, "Not authorized"));
+      throw HttpError(401, "Not authorized");
     }
 
     req.user = user;
     next();
   } catch (error) {
-    next(HttpError(401, error.message));
+    next(error);
   }
 };
+
+// const authenticate = async (req, res, next) => {
+//   try {
+//     const { authorization } = req.headers;
+//     const [bearer, token] = authorization.split(" ");
+//     if (bearer !== "Bearer" || token === null) {
+//       throw HttpError(401, "Not authorized");
+//     }
+
+//     try {
+//       const { id } = jwt.verify(token, SECRET);
+//       const user = await User.findById(id);
+//       if (!user || !user.token || user.token !== token) {
+//         throw HttpError(401, "Not authorized");
+//       }
+
+//       req.user = user;
+//       next();
+//     } catch (error) {
+//       throw HttpError(401, error.message);
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 export default authenticate;
